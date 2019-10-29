2a7282d17b2d2b7fa7771275c30e0b2a40094dc5
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-10-29 15:04:24 -0700

    added a google endpoint

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const auth = require('./routes/auth');
+const passportSetup = require('./config/passport-google-setup');
 
 const app = express();
 const bodyParser = require('body-parser');
