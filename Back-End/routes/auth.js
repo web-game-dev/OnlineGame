d5bcc25e41214c984e5e6015bb2dfb8e5b08807b
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-10-29 14:53:13 -0700

    added a route to send user to google's authentication

--- Back-End/routes/auth.js
+++ Back-End/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 router.get('/login', (req, res) => {
   console.log('login');
 });
+
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}));
 
 module.exports = router;
