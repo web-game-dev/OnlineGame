e81ef158f495530430aab8be11c510b84f3541d0
AbbyLan <lindalan4891@gmail.com>
2019-11-03 17:45:39 -0800

    #12 login page

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.get('/signup', (req,res) => res.render('pages/register'));
+//app.get('/registerDone', (req,res) => res.render('pages/index'));
+app.get('/registerDone', (req,res) => res.redirect('pages/index'));
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
