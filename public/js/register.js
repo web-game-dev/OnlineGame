d1ea3e7efa62e694fc1fddd604b212556f86c0f6
AbbyLan <lindalan4891@gmail.com>
2019-11-04 12:45:54 -0800

    update

---
+++ public/js/register.js
@@ -0,0 +1,16 @@
+const email = document.getElementById("email").value;
+const name = document.getElementById("name").value;
+const password = document.getElementById("password").value;
+
+//https://dungeon-crawler-back-end.herokuapp.com/auth/register
+
+// document.getElementById("regist").addEventListener("click", myFunction);
+//
+// function myFunction() {
+//   document.getElementById("regist").innerHTML = "Sign Up Successful!";
+// }
+
+var regBtn = document.getElementById("regRtn");
+regBtn.onclick = function(){
+
+}
