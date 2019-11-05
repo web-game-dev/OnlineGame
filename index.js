07e7f4c426537226e4bd6b01068aeeb5cb3b5ef2
Y. Brian Lee <yx.b.lee@gmail.com>
2019-11-04 16:24:17 -0800

    [15] Added connections to database (post requests); modified formatting

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const axios = require('axios')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
@@ -11,19 +13,67 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/signIn'));
+app.get('/signin', (req, res) => res.render('pages/signIn'));
 app.get('/signup', (req,res) => res.render('pages/register'));
 app.get('/demo', (req,res) => res.render('pages/demo'));
 
-// app.get('/registerDone', (req,res) => res.redirect('/'));
+app.post('/login', (req, res) => {
+    const register_uri = "https://dungeon-crawler-back-end.herokuapp.com/auth/login"; 
+    
+    var name = req.body.username;
+    var email = req.body.email;
+    var pw = req.body.password;
 
-app.post('/register', (req, res) => {
-	var name = req.body.username
-	var email = req.body.email;
-	var pw = req.body.password;
-	console.log(email, pw);
-	res.send(`Hello, ${name}. Email: ${email}. PW: ${pw}.`);
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pw, 
+    }
+    
+    // console.log("DATA is " + JSON.stringify(data));
+    
+    axios.post(register_uri, data)
+      .then(res => {
+        console.log(`statusCode: ${res.status}, ${res.statusText}`)
+        // console.log(res)
+      })
+      .catch(error => {
+        console.error(error)
+      });
+
+    token = res.data;
+    console.log(token);
+    if (!res.isAxiosError) res.redirect("/demo");
 });
 
-// app.send("signupAction", (req,res) => req.body.email, req.body.password);
+app.post('/register', (req, res) => {
+    const register_uri = "https://dungeon-crawler-back-end.herokuapp.com/auth/register"; 
+    
+    var name = req.body.username
+    var email = req.body.email;
+    var pw = req.body.password;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pw, 
+    }
+    
+    // console.log("DATA is " + JSON.stringify(data));
+    
+    axios.post(register_uri, data)
+      .then(res => {
+        console.log(`statusCode: ${res.status}, ${res.statusText}`)
+        console.log(res)
+      })
+      .catch(error => {
+        console.error(error)
+      });
+
+    token = res.data;
+    console.log(token);
+    if (!res.isAxiosError) res.redirect("/demo");
+});
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
