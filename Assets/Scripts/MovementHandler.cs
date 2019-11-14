a4377610308d30dbe59c05e4bce92b396a60d200
dayvidpham <dayvidpham@gmail.com>
2019-11-14 01:47:18 -0800

    Simple Unity game that shows off basic movement, animations, tilesets, and collisions; purely for learning purposes and not to be used in dev

---
+++ Assets/Scripts/MovementHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHandler : MonoBehaviour
+{
+    [SerializeField] private float speed = 6f;  // character walks at this constant speed
+    private Vector3 moveDir;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        // Initialize movement vars to 0
+        this.moveDir = new Vector3(0f, 0f);
+        this.anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Helper function to flip direction that character is facing
+    private void FlipPlayerDir()
+    {
+        // Flip direction that character is facing
+        Vector3 tempScale = this.transform.localScale;
+        tempScale.x *= -1;
+        this.transform.localScale = tempScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.moveDir.x = Input.GetAxisRaw("Horizontal");
+        this.moveDir.y = Input.GetAxisRaw("Vertical");
+        moveDir.Normalize();
+
+        // If moveDir is non-zero, play walking animation in correct dir
+        if ( moveDir.magnitude != 0f  )
+        {
+            // Determine direction of walking animation
+            bool wasFacingLeft = (this.transform.localScale.x < 0);
+
+            if ( this.moveDir.x < 0 && !wasFacingLeft )
+            {
+                // moving left, previously facing right
+                FlipPlayerDir();
+            } else if ( this.moveDir.x > 0 && wasFacingLeft ) {
+                // moving right, previously facing left
+                FlipPlayerDir();
+            }
+
+            // Activate Walk animation
+            anim.SetBool("isWalking", true);
+        } else {
+            anim.SetBool("isWalking", false);
+        }
+ 
+        this.transform.position += (this.moveDir * this.speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private SpriteRenderer spriteRenderer;
+    private int currentFrame;
+    private float timer;
+    private float framerate = 0.06666f;
+
+
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= framerate)
+        {
+            timer -= framerate;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            spriteRenderer.sprite = frameArray[currentFrame];
+        }
+    }
+}
