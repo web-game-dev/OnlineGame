d911fc97a1102bf4af42702fcc82c1278d079cff
Y. Brian Lee <yx.b.lee@gmail.com>
2019-11-14 22:51:27 -0800

    Complete basic express-session implementation, include modularized authorization action, add logout action

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,12 +1,13 @@
+/*** Variables & System Settings ***/
 const express = require('express')
 const session = require('express-session');
 const path = require('path')
 // const MongoClient = require('mongodb').MongoClient;
 const axios = require('axios')
 
-/*** Variable & Express Settings ***/
 const PORT = process.env.PORT || 5000
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,29 +15,29 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true,
-    cookie: { secure: true },
-    loggedin: false
+    // cookie: { secure: true },
 }));
-// let token = "";
+/*******/
+
+/*** Authentication & Authorization Middleware ***/
+const auth = function(req, res, next) {
+    console.log(req.session);
+    if (req.session && req.session.loggedin && req.session.email) return next();
+    else return res.status(401).send("Unauthorized. Please log in and try again.");;
+};
 /*******/
 
 /*** Views Rendering ***/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    if (req.session.loggedin) {
-        res.send('Welcome back, ' + req.session.email + '!');
-        res.render("pages/demo");
-    } else {
-        res.send('Please login to view this page!');
-        res.render('pages/signIn')
-    }
-    res.end();
-});
+app.get('/', (req, res) => res.render('pages/signIn'));
 app.get('/signin', (req, res) => res.render('pages/signIn'));
 app.get('/signup', (req,res) => res.render('pages/register'));
-// app.get('/demo', (req,res) => res.render('pages/demo'));
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.send("Logout Successful!");
+});
+app.get('/game', auth, (req, res) => res.render("pages/demo"));
 /*******/
 
 /*** POST Requests ***/
@@ -54,20 +55,21 @@ app.post('/login', async (req, res) => {
     // console.log("DATA is " + JSON.stringify(data));
 
     await axios.post(login_uri, data)
-          .then(axiosResp => {
-            console.log(axiosResp);
+        .then(axiosResp => {
+            // console.log(axiosResp);
             console.log(`statusCode: ${axiosResp.status}, ${axiosResp.statusText}`);
             req.session.loggedin = true;
             req.session.secret = axiosResp.data;
             req.session.email = email;
-            res.render("pages/demo");
-          })
-          .catch(error => {
+            // console.log(req.session);
+            res.redirect("/game");
+        })
+            .catch(error => {
             console.error("ERROR:", error.response);
             // alert(error.response.data);         
-            res.status(401).send(error.response.data);
+            res.status(401).send(error.response.data + ". Please return to the previous page and try again.");
             // res.redirect('/#error');
-          });
+        });
 });
 
 // Registration
@@ -84,23 +86,23 @@ app.post('/register', async (req, res) => {
     // console.log("DATA is " + JSON.stringify(data));
 
     await axios.post(register_uri, data)
-      .then(axiosResp => {
-        console.log(axiosResp);
-        console.log(`statusCode: ${axiosResp.status}, ${axiosResp.statusText}`);
-        req.session.loggedin = true;
-        req.session.secret = axiosResp.data;
-        req.session.username = name;
-        req.session.email = email;
-        res.render("pages/demo");
-      })
-      .catch(error => {
-        // console.error(error)
-        console.error("ERROR:", error.response);
-        // alert("ERROR:", error.response.data);
-        res.status(401).end(error.response.data);
-        // res.redirect('/signup/#error');
-      });
+        .then(axiosResp => {
+            // console.log(axiosResp);
+            console.log(`statusCode: ${axiosResp.status}, ${axiosResp.statusText}`);
+            req.session.loggedin = true;
+            req.session.secret = axiosResp.data;
+            req.session.name = name;
+            req.session.email = email;
+            // console.log(req.session);
+            res.redirect("/game");
+        })
+        .catch(error => {
+            console.error("ERROR:", error.response);
+            // alert("ERROR:", error.response.data);
+            res.status(401).end(error.response.data + ". Please return to the previous page and try again.");
+            // res.redirect('/signup/#error');
+        });
 });
 /*******/
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }...`))
