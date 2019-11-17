2c627929cdc599c98a017acbb7252730905620ec
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-11-17 03:25:27 -0800

    [16] added access control header to requests

--- Back-End/index.js
+++ Back-End/index.js
@@ -11,6 +11,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(helmet());
 app.use(compression());
 app.use(cors());
