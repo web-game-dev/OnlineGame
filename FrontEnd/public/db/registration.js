c6e91d5fa34e7ebebd64829e80b73f69118fc893
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-11-17 01:39:25 -0800

    [16] refactored the code to make it cleaner

--- FrontEnd/public/db/registration.js
+++ FrontEnd/public/db/registration.js
@@ -1,21 +1,4 @@
-function validate (input) {
-  if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-    if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-      return false;
-    }
-  }
-  else {
-    if($(input).val().trim() == ''){
-      return false;
-    }
-  }
-  return true;
-}
-
-function showValidate(input) {
-  var thisAlert = $(input).parent();
-  $(thisAlert).addClass('alert-validate');
-}
+import { validate, showValidate } from '../utility/validation'
 
 async function postUser() {
   const data = {};
