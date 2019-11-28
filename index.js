0c168606e55272a768764cddad2cb1029b5cf9b4
Y. Brian Lee <yx.b.lee@gmail.com>
2019-11-28 07:34:40 -0800

    [25] Convert relevant OAuth links to relative paths and actions; functionality tests pass for port 3000; add req path to render chatbox; minor formatting

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const session = require('express-session');
-const path = require('path')
-const axios = require('axios')
+const path = require('path');
+const axios = require('axios');
 const config = require('config');
 const passportSetup = require('./back-end/oauthStrategy/passport-google-strategy');
 const authRoute = require('./back-end/routes/auth');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -32,7 +32,7 @@ app.use('/auth', authRoute);
 /*******/
 
 /*** Server Connection ***/
-const server = http.listen(PORT, () => console.log(`Listening on ${ PORT }...`))
+const server = http.listen(PORT, () => console.log(`Listening on ${ PORT }...`));
 /*******/
 
 /*** MongoDB Connections ***/
@@ -40,10 +40,10 @@ if (process.env.NODE_ENV === 'development') {
   const db = config.get('db');
   mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
-      console.log(`Connected to db...`);
+      console.log(`Connected to MongoDB...`);
     })
     .catch(err => {
-      console.log('Could not connect to mongodb', err);
+      console.log('Could not connect to MongoDB', err);
     });
 }
 
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'test') {
       console.log(`Connected to ${db}...`);
     })
     .catch(err => {
-      console.log('Could not connect to mongodb', err);
+      console.log('Could not connect to MongoDB', err);
     });
 }
 /*******/
@@ -64,16 +64,13 @@ io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
         io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-    }); //if online
-
+    });
     socket.on('disconnect', function(username) {
             io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-        }) //if offline
-
+    });
     socket.on('chat_message', function(message) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-    }); //text message
-
+    });
 });
 /*******/
 
@@ -95,7 +92,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     if (req.session.loggedin) res.redirect('demo');
-    else res.redirect('/signin')
+    else res.redirect('/signin');
 });
 app.get('/signin', loggedInAlert, (req, res) => res.render('pages/signIn'));
 app.get('/signup', loggedInAlert, (req,res) => res.render('pages/register'));
@@ -105,6 +102,8 @@ app.get('/logout', function (req, res) {
     res.redirect('/signin');
 });
 app.get('/demo', auth, (req, res) => res.render("pages/demo", { name: req.session.name}));
+app.get('/game', auth, (req, res) => res.render("pages/demo", { name: req.session.name}));
+app.get('/chatbox', auth, (req, res) => res.render("pages/chatbox"));
 /*******/
 
 module.exports = server;
