faeebde6906294cff72ec076d5b4a5a838942252
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-12-01 01:57:56 -0800

    [33] added update endpoint for player information

--- back-end/routes/players.js
+++ back-end/routes/players.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const tokenDecode = require('../util/tokenDecode');
+const _ = require('lodash');
 const { auth } = require('../middleware/auth');
-const { Player } = require('../models/player');
+const { Player, validatePlayer } = require('../models/player');
 
 const router = express.Router();
 
@@ -10,7 +11,6 @@ router.post('/create', auth, async (req, res) => {
   const id = tokenDecode(req)._id;
 
   let player = await Player.findOne({ userId: id});
-  console.log('player exists', player);
   if (player) return res.status(400).send('player already exists');
 
   player = new Player({
@@ -23,14 +23,40 @@ router.post('/create', auth, async (req, res) => {
     .catch(err => {
       console.log(err);
     });
-  console.log('player created', result);
+  const payload = _.pick(result, ['health', 'exp', 'level']);
+  res.send(payload);
 });
 
 // Retrieve player that is logged in
 router.get('/get', auth, async (req, res) => {
   const id = tokenDecode(req)._id;
   const player = await Player.findOne({ userId: id}).select('-_id -userId -__v');
+  if (!player) return res.status(404).send('Player not found');
   res.send(player);
+});
+
+// updates the player's information
+router.put('/update', auth, async (req, res) => {
+  const { error } = validatePlayer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const id = tokenDecode(req)._id;
+
+  let player = Player.findOne({ userId: id});
+  if(!player) return res.status(404).send('Player not found');
+
+  const { health, exp, level } = req.body;
+  player = await Player.findOneAndUpdate({ userId: id}, {
+    health: health,
+    exp: exp,
+    level: level,
+    userId: id,
+  }, { new: true })
+    .catch(err => {
+      return res.status(500).send('Could not update player information');
+    });
+  const payload = _.pick(player, ['health', 'exp', 'level']);
+  console.log(payload);
+  res.send(payload);
 });
 
 module.exports = router;
