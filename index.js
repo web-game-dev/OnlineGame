e355e921e960631aa646445027482a65f3b56757
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-12-01 01:58:39 -0800

    [33] fixed decpracated warning with mongoose

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const players = require('./back-end/routes/players');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-const httpServer = http.Server(app);
-const io = socketIO(httpServer);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+mongoose.set('useFindAndModify', false);
 
 if (!config.get('sessionSecret')) {
   console.log('FATAL sessionSecret IS MISSING');
   process.exit(1);
 }
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.json());
